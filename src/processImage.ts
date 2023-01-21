27a66afa7745d8e894dabebe662db7bc410efcbf
Yousif Abozid <yousif.abozid@yahoo.com>
2023-01-21 22:06:46 +0200

    finish the image process and the app is done

---
+++ src/processImage.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import sharp from 'sharp'
+import { promises as fsPromises } from 'fs'
+import fs from 'fs'
+
+// Resize the image and save it in the thumb folder
+const processImage = async (
+  filename: string,
+  width: number,
+  height: number
+) => {
+  const image = path.join(__dirname, '../assets/full', `${filename}.jpg`)
+  const thumbFolder: string = path.join(__dirname, '../assets/thumb')
+  const imageOutput: string =
+    path.join(__dirname, '../assets/thumb', filename) +
+    `-${width}-${height}.jpg`
+
+  // If thumb folder doesn't exist, create it
+  if (!fs.existsSync(thumbFolder)) {
+    await fsPromises.mkdir(thumbFolder)
+  }
+
+  try {
+    // Resize the image and return the resized image
+    await sharp(image).resize(width, height).toFile(imageOutput)
+    return imageOutput
+  } catch (error) {
+    // If it fails, returns the error
+    return error
+  }
+}
+
+export default processImage
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,28 +1,94 @@
 import express from 'express'
 import fs from 'fs'
 import path from 'path'
+import processImage from '../processImage'
 
 const routes = express.Router()
 
 // Root endpoint`
 routes.get('/', (req, res) => res.send('Server Is Running!'))
 
-routes.get('/images', (req, res) => {
-  if (req.query.filename) {
-    const filename = req.query.filename as string
+// Images endpoint
+routes.get('/images', async (req, res) => {
+  // Get all images from the assets folder
+  const images = fs.readdirSync(path.join(__dirname, '../../assets/full'))
+  const imageNames = images.map((image) => image.split('.')[0])
+
+  // Get the query params
+  const { filename, width, height } = req.query
+  const filenameToString = String(filename)
+  const widthToNumber = Number(width)
+  const heightToNumber = Number(height)
+
+  // checks if the image exists
+  if ((filename && !width) || !height) {
+    // get the image from the assets folder
     const image = path.join(
       __dirname,
-      '../',
-      '../',
-      'assets/',
-      'full/',
-      `${filename}.jpg`
+      '../../assets/full',
+      `${filenameToString}.jpg`
     )
-
+    // If the image exists, send it
     if (fs.existsSync(image)) {
       res.sendFile(image)
     } else {
-      res.send('There is no image with that name')
+      // If the image doesn't exist, return all avilable images links
+      res.send(
+        `<div>There is no image with that name (${
+          filenameToString ? filenameToString : ''
+        }) <br> <br> Available images to search: <br>
+        ${imageNames.map(
+          (image) =>
+            `<br>
+          <a href="http://localhost:3000/images?filename=${image}">http://localhost:3000/images?filename=${image}</a>`
+        )}
+        <br> <br> Or search with width and height: <br>
+        ${imageNames.map(
+          (image) =>
+            `<br>
+            <a href="http://localhost:3000/images?filename=${image}&width=100&height=100">http://localhost:3000/images?filename=${image}&width=100&height=100</a></a>`
+        )}
+        </div>`
+      )
+    }
+  } else if (filename && width && height) {
+    const image = path.join(
+      __dirname,
+      '../../assets/thumb',
+      `${filenameToString}-${widthToNumber}-${heightToNumber}.jpg`
+    )
+
+    try {
+      // If the image exists, send it
+      if (fs.existsSync(image)) {
+        res.sendFile(image)
+      } else {
+        const resizedImage = await processImage(
+          filenameToString,
+          widthToNumber,
+          heightToNumber
+        )
+        res.sendFile(resizedImage as string)
+      }
+    } catch (error) {
+      // If the image doesn't exist, return all avilable images links
+      res.send(
+        `<div>There is no image with that name (${
+          filenameToString ? filenameToString : ''
+        }) <br> <br> Available images to search: <br>
+        ${imageNames.map(
+          (image) =>
+            `<br>
+          <a href="http://localhost:3000/images?filename=${image}">http://localhost:3000/images?filename=${image}</a>`
+        )}
+        <br> <br> Or search with width and height: <br>
+        ${imageNames.map(
+          (image) =>
+            `<br>
+            <a href="http://localhost:3000/images?filename=${image}&width=100&height=100">http://localhost:3000/images?filename=${image}&width=100&height=100</a></a>`
+        )}
+        </div>`
+      )
     }
   }
 })
